2c1c582cdab0f5049aca198b34c67b383e961e98
albjimmar1 <47810351+albjimmar1@users.noreply.github.com>
2023-05-08 21:09:52 +0200

    Main path to Swagger

--- PeopleOverview/Models/Person.cs
+++ PeopleOverview/Models/Person.cs
@@ -1,16 +1,24 @@
-namespace PeopleOverview.Models;
+ using Microsoft.EntityFrameworkCore;
 
-public class Person
+namespace PeopleOverview.Models
 {
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Surnames { get; set; }
-    public int? Age { get; set; }
-    public string? CountryOfBirth { get; set; }
-    public string? CountryOfResidence { get; set; }
-    public string? Job { get; set; }
-    public bool? IsSatisfiedWithWork { get; set; }
-    public decimal? GrossAnnualSalary { get; set; }
-    public string? Currency { get; set; } 
-    public bool? IsSatisfiedWithSalary { get; set; }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surnames { get; set; }
+        public int? Age { get; set; }
+        public string? CountryOfBirth { get; set; }
+        public string? CountryOfResidence { get; set; }
+        public string? Job { get; set; }
+        public bool? IsSatisfiedWithWork { get; set; }
+        public decimal? GrossAnnualSalary { get; set; }
+        public string? Currency { get; set; } 
+        public bool? IsSatisfiedWithSalary { get; set; }
+    }
+    class PersonDb : DbContext
+    {
+        public PersonDb(DbContextOptions options) : base(options) { }
+        public DbSet<Person> People { get; set; } = null!;
+    }
 }
--- PeopleOverview/Program.cs
+++ PeopleOverview/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using PeopleOverview.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +9,52 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<PersonDb>(options => options.UseInMemoryDatabase("item"));
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "People overview API",
+        Description = "An ASP.NET Core Web API for managing people items",
+        /*TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Example Contact",
+            Url = new Uri("https://example.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Example License",
+            Url = new Uri("https://example.com/license")
+        }*/
+    });
+});
+
+const string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+      builder =>
+      {
+          builder.WithOrigins("*");
+      });
+});
 
 var app = builder.Build();
 
+app.UseCors(MyAllowSpecificOrigins);
+
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || true)
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
@@ -22,10 +63,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-/*app.UseSwagger();
-app.UseSwaggerUI(c =>
+/*app.MapGet("/pizzas", async (PersonDb db) => await db.People.ToListAsync());
+
+app.MapPost("/pizza", async (PersonDb db, Person person) =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PeopleOverview API V1");
+    await db.People.AddAsync(person);
+    await db.SaveChangesAsync();
+    return Results.Created($"/pizza/{person.Id}", person);
 });*/
 
 app.Run();
